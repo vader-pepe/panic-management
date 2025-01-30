c535456122c07cbc01fa8a3145b66278e205d808
Ihsan Kurniawan <ihsan@localhost.localdomain>
2025-01-30 14:02:34 +0700

    feat: new game engine

--- scripts/settings.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-
-func _ready() -> void:
-	var btn = $"."
-	btn.pressed.connect(self._goto_settings_menu)
-
-func _goto_settings_menu():
-	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
--- scripts/start.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-func _ready() -> void:
-	var btn = $"."
-	btn.pressed.connect(self._goto_level_scene)
-	
-func _goto_level_scene():
-	get_tree().change_scene_to_file("res://scenes/level1.tscn")
